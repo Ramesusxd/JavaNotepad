e0d8bd5e4fb0a5f659029638df3956f42d7b1fca
RamesusXD <j1gamer4life@gmail.com>
2020-12-15 17:07:43 -0800

    Update to source code Updated: - added Function_Format.java class to let a user set the font and size of the font -added Function_Color.java class to let the user change the background color of the note pad

---
+++ src/GUI/Function_Color.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import java.awt.*;
+
+public class Function_Color {
+
+    GUI gui;
+
+    public Function_Color(GUI gui) {
+
+        this.gui = gui;
+    }
+
+    public void changeColor(String color) {
+
+        switch(color) {
+            case "White":
+                gui.window.getContentPane().setBackground(Color.white);
+                gui.textArea.setBackground(Color.white);
+                gui.textArea.setForeground(Color.black);
+                break;
+            case "Black":
+                gui.window.getContentPane().setBackground(Color.black);
+                gui.textArea.setBackground(Color.black);
+                gui.textArea.setForeground(Color.white);
+                break;
+            case "Blue":
+                gui.window.getContentPane().setBackground(Color.blue);
+                gui.textArea.setBackground(Color.blue);
+                gui.textArea.setForeground(Color.white);
+                break;
+        }
+
+
+    }
+
+}
--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -17,13 +17,25 @@ import java.awt.event.ActionListener;
 public class GUI implements ActionListener {
 
     JFrame window;
+
+    // Text area
     JTextArea textArea;
+    Boolean wordWrapOn = false;
+    // menu bar
     JScrollPane scrollPane;
     JMenuBar menuBar;
     JMenu menuFile, menuEdit, menuFormat, menuColor;
+    // File
     JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
+    // Format
+    JMenuItem iWrap, iFontArial, iFontCSMS, iFontTNR, iFontSize8, iFontSize12, iFontSize16, iFontSize20, iFontSize24, iFontSize28;
+    JMenu menuFont, menuFontSize;
+    // color
+    JMenuItem iColor1, iColor2, iColor3;
 
     Function_File file = new Function_File(this);
+    Function_Format format = new Function_Format(this);
+    Function_Color color = new Function_Color(this);
 
 
     public static void main(String[] args) {
@@ -37,6 +49,13 @@ public class GUI implements ActionListener {
         createTextArea();
         createMenuBar();
         createFileMenu();
+        createFormatMenu();
+        createColorMenu();
+
+        format.selectedFont = "Arial";
+        format.createFont(16);
+        format.wordWrap();
+        color.changeColor("White");
         window.setVisible(true);
 
     }
@@ -101,6 +120,82 @@ public class GUI implements ActionListener {
         menuFile.add(iExit);
 
     }
+    public void createFormatMenu() {
+
+        iWrap = new JMenuItem("Word Wrap: Off");
+        iWrap.addActionListener(this);
+        iWrap.setActionCommand("Word Wrap");
+        menuFormat.add(iWrap);
+
+        menuFont = new JMenu("Font");
+        menuFormat.add(menuFont);
+
+        iFontArial = new JMenuItem("Arial");
+        iFontArial.addActionListener(this);
+        iFontArial.setActionCommand("Arial");
+        menuFont.add(iFontArial);
+
+        iFontCSMS = new JMenuItem("Comic Sans MS");
+        iFontCSMS.addActionListener(this);
+        iFontCSMS.setActionCommand("Comic Sans MS");
+        menuFont.add(iFontCSMS);
+
+        iFontTNR = new JMenuItem("Times New Roman");
+        iFontTNR.addActionListener(this);
+        iFontTNR.setActionCommand("Times New Roman");
+        menuFont.add(iFontTNR);
+
+        menuFontSize = new JMenu("Font Size");
+        menuFormat.add(menuFontSize);
+
+        iFontSize8 = new JMenuItem("8");
+        iFontSize8.addActionListener(this);
+        iFontSize8.setActionCommand("Size8");
+        menuFontSize.add(iFontSize8);
+
+        iFontSize12 = new JMenuItem("12");
+        iFontSize12.addActionListener(this);
+        iFontSize12.setActionCommand("Size12");
+        menuFontSize.add(iFontSize12);
+
+        iFontSize16 = new JMenuItem("16");
+        iFontSize16.addActionListener(this);
+        iFontSize16.setActionCommand("Size16");
+        menuFontSize.add(iFontSize16);
+
+        iFontSize20 = new JMenuItem("20");
+        iFontSize20.addActionListener(this);
+        iFontSize20.setActionCommand("Size20");
+        menuFontSize.add(iFontSize20);
+
+        iFontSize24 = new JMenuItem("24");
+        iFontSize24.addActionListener(this);
+        iFontSize24.setActionCommand("Size24");
+        menuFontSize.add(iFontSize24);
+
+        iFontSize28 = new JMenuItem("28");
+        iFontSize28.addActionListener(this);
+        iFontSize28.setActionCommand("Size28");
+        menuFontSize.add(iFontSize28);
+    }
+    public void createColorMenu() {
+
+        iColor1 = new JMenuItem("White");
+        iColor1.addActionListener(this);
+        iColor1.setActionCommand("White");
+        menuColor.add(iColor1);
+
+        iColor2 = new JMenuItem("Black");
+        iColor2.addActionListener(this);
+        iColor2.setActionCommand("Black");
+        menuColor.add(iColor2);
+
+        iColor3 = new JMenuItem("Blue");
+        iColor3.addActionListener(this);
+        iColor3.setActionCommand("Blue");
+        menuColor.add(iColor3);
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -113,6 +208,19 @@ public class GUI implements ActionListener {
             case "Save": file.save(); break;
             case "SaveAs": file.saveAs(); break;
             case "Exit": file.exit(); break;
+            case "Word Wrap": format.wordWrap(); break;
+            case "Arial": format.setFont(command); break;
+            case "Comic Sans MS": format.setFont(command); break;
+            case "Times New Roman": format.setFont(command); break;
+            case "Size8": format.createFont(8); break;
+            case "Size12": format.createFont(12); break;
+            case "Size16": format.createFont(16); break;
+            case "Size20": format.createFont(20); break;
+            case "Size24": format.createFont(24); break;
+            case "Size28": format.createFont(28); break;
+            case "White": color.changeColor(command); break;
+            case "Black": color.changeColor(command); break;
+            case "Blue": color.changeColor(command); break;
 
         }
 
